7b88559a2bce344ec363f7751ee0cbae46ba9a00
Mihai Simion <mihai@simion.dev>
2022-01-10 23:50:34 +0200

    changed output path to js/compiled

--- project.clj
+++ project.clj
@@ -37,13 +37,13 @@
                            :source-map-timestamp true
                            ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
                            ;; https://github.com/binaryage/cljs-devtools
-                           :preloads [devtools.preload]}}
+                           #_:preloads #_[devtools.preload]}}
                ;; This next build is a compressed minified build for
                ;; production. You can build this with:
                ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/infinitexo.js"
+                :compiler {:output-to "js/compiled/infinitexo.js"
                            :main infinitexo.core
                            :optimizations :advanced
                            :pretty-print false}}]}
