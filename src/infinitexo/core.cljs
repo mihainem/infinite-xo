6a01e5cebe19149f8bb0b4f2828226d0666a0963
Mihai Simion <mihai@simion.dev>
2022-01-11 23:34:49 +0200

    added displaying of winner-player as printed to console

--- src/infinitexo/core.cljs
+++ src/infinitexo/core.cljs
@@ -1,11 +1,11 @@
 (ns infinitexo.core
-    (:require 
-              [reagent.core :as reagent :refer [atom]]
-              [reagent.dom :as rd]))
+  (:require
+   [reagent.core :as reagent :refer [atom]]
+   [reagent.dom :as rd]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/infinitexo/core.cljs. Go ahead and edit it and see reloading in action.")
+;;(println "This text is printed from src/infinitexo/core.cljs. Go ahead and edit it and see reloading in action.")
 
 ;; define your app data so that it doesn't get over-written on reload
 
@@ -15,7 +15,7 @@
 
 (def app-state (atom {:text "Infinite XO"
                       :board (new-board 10)
-                      :current-player 0
+                      :current-player -1
                       :current-state "B"}))
 
 (defn computer-move []
@@ -25,91 +25,96 @@
   (swap! app-atom update-in [:current-state] inc))
 
 (def o [:svg
-    {:view-box "0 0 30 30"
-     :width 500
-     :height 500}
-     [:circle {:r 30 :cx 30 :cy 30}]])
+        {:view-box "0 0 30 30"
+         :width 500
+         :height 500}
+        [:circle {:r 30 :cx 30 :cy 30}]])
 
-(def active-users 
-  [{:name "Player 1" 
+
+(def active-users
+  [{:name "Player 1"
     :symbol "X"
     :total-wins 0}
-   {:name "Player 2" 
+   {:name "Player 2"
     :symbol "O"
     :total-wins 0}
-   {:name "Player 3" 
-    :symbol "I"
-    :total-wins 0}])
+   #_{:name "Player 3"
+      :symbol "I"
+      :total-wins 0}])
+
+(defn display-winner [symbol]
+  (let [winning-player (->> active-users
+                            (filter #(= (:symbol %) symbol))
+                            (first)
+                            (:name))]
+    (when (not (nil? winning-player)) (println winning-player "has won"))))
 
 (defn check-solution [i j]
   (let [board (get-in @app-state [:board])
         valids-nr 5
-        valid? (fn [arr] (let 
-                          [groups (partition-by identity arr)] 
-                           (println groups)
-                           (true? (some #(and (>= (count %) valids-nr) 
-                                              (not= (first %) "B"))
-                                       groups ))))
-        horrizontal (vec (for [x (map #(+ (- i (dec valids-nr)) %) (range (dec (* 2 valids-nr)))) 
+        valid? (fn [arr] (let
+                          [groups (partition-by identity arr)
+                           filtered-groups (filter #(and (>= (count %) valids-nr)
+                                                         (not= (first %) "B")) groups)]
+                           (if (> (count filtered-groups) 0) (first (first filtered-groups)) nil)
+                           ))
+        horrizontal (vec (for [x (map #(+ (- i (dec valids-nr)) %) (range (dec (* 2 valids-nr))))
                                :when (<= 0 x (dec (count board)))]
                            (get-in board [j x])))
-        vertical (vec (for [x (map #(+ (- j (dec valids-nr)) %) (range (dec (* 2 valids-nr)))) 
-                               :when (<= 0 x (dec (count board)))]
-                           (get-in board [x i])))
-        left-diagonal (vec (for [x (map #(+ (- (dec valids-nr)) %) (range (dec (* 2 valids-nr)))) 
-                               :when (and (<= 0 (+ x j) (dec (count board)))
-                                          (<= 0 (+ x i) (dec (count board)))
-                               )]
-                           (get-in board [(+ j x) (+ i x)])))
-        right-diagonal (vec (for [x (map #(+ (- (dec valids-nr)) %) (range (dec (* 2 valids-nr)))) 
-                               :when (and (<= 0 (- j x) (dec (count board)))
-                                          (<= 0 (+ i x) (dec (count board)))
-                               )]
-                           (get-in board [(- j x) (+ i x)])))]
-    (true? (some true? (map valid? [horrizontal vertical left-diagonal right-diagonal]))
-    )))
+        vertical (vec (for [x (map #(+ (- j (dec valids-nr)) %) (range (dec (* 2 valids-nr))))
+                            :when (<= 0 x (dec (count board)))]
+                        (get-in board [x i])))
+        left-diagonal (vec (for [x (map #(+ (- (dec valids-nr)) %) (range (dec (* 2 valids-nr))))
+                                 :when (and (<= 0 (+ x j) (dec (count board)))
+                                            (<= 0 (+ x i) (dec (count board))))]
+                             (get-in board [(+ j x) (+ i x)])))
+        right-diagonal (vec (for [x (map #(+ (- (dec valids-nr)) %) (range (dec (* 2 valids-nr))))
+                                  :when (and (<= 0 (- j x) (dec (count board)))
+                                             (<= 0 (+ i x) (dec (count board))))]
+                              (get-in board [(- j x) (+ i x)])))]
+    (->> [horrizontal vertical left-diagonal right-diagonal]
+         (map valid?)
+         (remove nil?)
+         (first)
+         (display-winner))))
 
 (defn blank [i j]
-        [:rect {:width 0.95
-              :height 0.95
-              :fill "gray"
-              :x i
-              :y j
-              :on-click (fn rect-click [] 
-                            (swap! app-state update-in [:current-player] #(mod (inc %) (count active-users)))
-                            (swap! app-state assoc-in [:current-state] (get-in active-users [(get-in @app-state [:current-player]) :symbol]))
-                            (swap! app-state assoc-in [:board j i] (get-in @app-state [:current-state]))
-                            (println (check-solution i j))
-                          )}])
-                           
+  [:rect {:width 0.95
+          :height 0.95
+          :fill "gray"
+          :x i
+          :y j
+          :on-click (fn rect-click []
+                      (swap! app-state update-in [:current-player] #(mod (inc %) (count active-users)))
+                      (swap! app-state assoc-in [:current-state] (get-in active-users [(get-in @app-state [:current-player]) :symbol]))
+                      (swap! app-state assoc-in [:board j i] (get-in @app-state [:current-state]))
+                      (check-solution i j))}])
+
 
 (defn cross [i j]
-       [:g  {:stroke "black" :stroke-width 0.1
-             :stroke-linecap "round"
-             :transform 
-             (str "translate(" (+ i 0.2) "," (+ 0.2 j) ") 
-                    scale(0.6)")
-             }
-        [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
-        [:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
+  [:g  {:stroke "black" :stroke-width 0.1
+        :stroke-linecap "round"
+        :transform
+        (str "translate(" (+ i 0.2) "," (+ 0.2 j) ") 
+                    scale(0.6)")}
+   [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
+   [:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
 
 (defn line [i j]
-       [:g  {:stroke "black" :stroke-width 0.1
-             :stroke-linecap "round"
-             :transform 
-             (str "translate(" (+ i 0.2) "," (+ 0.2 j) ") 
-                    scale(0.6)")
-             }
-        [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
-        #_[:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
+  [:g  {:stroke "black" :stroke-width 0.1
+        :stroke-linecap "round"
+        :transform
+        (str "translate(" (+ i 0.2) "," (+ 0.2 j) ") 
+                    scale(0.6)")}
+   [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
+   #_[:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
 
 
 (defn circle [i j]
-        [:circle {:r 0.45
-              :fill "red"
-              :cx (+ i 0.5)
-              :cy (+ j 0.5)
-              }])
+  [:circle {:r 0.45
+            :fill "red"
+            :cx (+ i 0.5)
+            :cy (+ j 0.5)}])
 
 
 
@@ -118,21 +123,18 @@
    [:h1 (:text @app-state)]
    (let [n 10]
      [:svg
-    {:view-box [0 0 n n]
-     :width 400
-     :height 400
-     }
-    (for [
-      i (range n)
-      j (range n)] ;;[0 1 2 3 4] [0 1 2 3 4]
-      ^{:key (+ (* i n) j)}
-      (case (get-in @app-state [:board j i])
-        "B" [blank i j]
-        "X" [cross i j]
-        "I" [line i j]
-        "O" [circle i j]))])
-   [:p [:button {:on-click (fn new-game-click [e] (swap! app-state assoc :board (new-board 10)))} "New Game"]]
-   ])
+      {:view-box [0 0 n n]
+       :width 400
+       :height 400}
+      (for [i (range n)
+            j (range n)] ;;[0 1 2 3 4] [0 1 2 3 4]
+        ^{:key (+ (* i n) j)}
+        (case (get-in @app-state [:board j i])
+          "B" [blank i j]
+          "X" [cross i j]
+          "I" [line i j]
+          "O" [circle i j]))])
+   [:p [:button {:on-click (fn new-game-click [e] (swap! app-state assoc :board (new-board 10)))} "New Game"]]])
 
 (rd/render [infinitexo]
            (. js/document (getElementById "app")))
@@ -141,4 +143,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
